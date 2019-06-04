353b2d3c4424c5ed783a69d7770c05204c05fa34
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-04 14:25:35 -0600

    break HTML into separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
 
+import MessageList from "./MessageList.jsx";
+import ChatBar from "./ChatBar.jsx";
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+        <ChatBar />
+      </div>
     );
   }
 }
---
+++ src/ChatBar.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class ChatBar extends Component {
+  render() {
+    return (
+      <footer className="chatbar">
+        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+      </footer>
+    );
+  }
+}
