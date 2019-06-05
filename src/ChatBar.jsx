1d1eb5c45ef4a6a0068cba88123cae972f9f6c2a
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-04 22:38:59 -0600

    add defaultValue property of current user

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ export default class ChatBar extends Component {
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
