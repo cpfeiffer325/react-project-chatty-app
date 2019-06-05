5bff2ea5820720db2726be086a5bc44b846df2bf
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-04 23:16:29 -0600

    add simulate incoming message on componentDidMount

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import React, {Component} from 'react';
 import MessageList from "./MessageList.jsx";
 import ChatBar from "./ChatBar.jsx";
 
+  // postMessages() {
+  //   this.setState({currentUser: name});
+  // }
+
 class App extends Component {
   // set initial state of chatty app
   constructor (props) {
@@ -25,9 +29,18 @@ class App extends Component {
     };
   }
 
-  // postMessages() {
-  //   this.setState({currentUser: name});
-  // }
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
 
   render() {
     return (
