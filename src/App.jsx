a0125457946e087961684101ed1cd7c7fa310623
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-04 22:38:27 -0600

    add user data for testing and add render states for messagelist and chatbar files

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,42 @@ import MessageList from "./MessageList.jsx";
 import ChatBar from "./ChatBar.jsx";
 
 class App extends Component {
+  // set initial state of chatty app
+  constructor (props) {
+    super(props);
+    this.state = 
+    {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 1,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 2,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
+  // postMessages() {
+  //   this.setState({currentUser: name});
+  // }
+
   render() {
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <MessageList />
-        <ChatBar />
+        <MessageList messages = {this.state.messages}/>
+        <ChatBar currentUser = {this.state.currentUser.name}/>
       </div>
     );
   }
 }
 export default App;
+
