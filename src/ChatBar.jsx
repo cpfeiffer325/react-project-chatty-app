43ce4d8a5d1522959c5070bb9b6b22ed89c0a2f7
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-05 21:39:26 -0600

    add event handler to change user name

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 export default class ChatBar extends Component {
   render() {
     return (
-      <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser}/>
+      <footer className="chatbar" >
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={this.props.currentUser} onKeyPress={this.props.updateCurrentUser}/>
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.props.postMessage}/>
       </footer>
     );
