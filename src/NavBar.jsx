0cd79bd0e6d62cfc335289f63bb3103cfd4f5b0e
cpfeiffer325 <cpfeiffer325@gmail.com>
2019-06-07 11:10:46 -0600

    add condition to display user instead of users when there is only one connection

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -5,7 +5,7 @@ export default class NavBar extends Component {
     return (
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
-          <div className="numConnections">{this.props.count} Users Online</div>
+          <div className="numConnections">{this.props.count} {this.props.count === 1 ? "user online" : "users online"}</div>
         </nav>
     );
   }
